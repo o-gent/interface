a798aa7cca12a81fdce5935df5fb2f0ebae61eed
James Thornton <jamesthornton78@gmail.com>
2018-06-07 22:59:22 +0100

    Integration prep
    Only apparant issue: the aircraft does not appear to change position - probably due to the simulator

--- dronekit_functions.py
+++ dronekit_functions.py
@@ -9,242 +9,253 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
 import time, argparse
 
+# essentially WP position
 drone_position_notify = [0,0]
 
 def connection():
-    # sitl stuff   ###########################
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    print 'sitl started'
-    ##########################################
-    
-    # literally no clue what these 5 lines do but it might be needed but actually probably not
-    # Parse connection argument
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--connect", help="connection string")
-    args = parser.parse_args()
-    if args.connect:
-        connection_string = args.connect
-    
-    
-    # Connect to the Vehicle
-    print "Connecting"
-    # referenced elsewhere in the file
-    global vehicle
-    
-    # once heartbeat has been adjusted, timeout needs to be set to 5
-    # location of USB connection to pixhawk
-    # connection_string       = '/dev/ttyACM0'
-    
-    connection_string = sitl.connection_string()
-    vehicle = connect(connection_string, wait_ready=True) #heartbeat_timeout = 30)
-    
-    print 'connected to vehicle'
-    
-    print "Basic pre-arm checks"
-    # Don't let the user try to fly while autopilot is booting
-    if vehicle.mode.name == "INITIALISING":
-        print "Waiting for vehicle to initialise"
-        time.sleep(1)
-    while vehicle.gps_0.fix_type < 2:
-        print "Waiting for GPS...:", vehicle.gps_0.fix_type
-        time.sleep(1)
-    
-    
-    # need to sort out home location setting
-    
-# =============================================================================
-#     # set home location
-#     vehicle.home_location=vehicle.location.global_frame
-# =============================================================================
-    
-    # Copter should arm in GUIDED mode
-    vehicle.mode    = VehicleMode("GUIDED")
+	# sitl stuff   ###########################
+	import dronekit_sitl
+	sitl = dronekit_sitl.start_default()
+	print('sitl started')
+	##########################################
+	
+	# literally no clue what these 5 lines do but it might be needed but actually probably not
+	# Parse connection argument
+	# parser = argparse.ArgumentParser()
+	# parser.add_argument("-c", "--connect", help="connection string")
+	# args = parser.parse_args()
+	# if args.connect:
+	#     connection_string = args.connect
+
+	# print("Args:", args)
+	# print("Connection string:", connection_string)
+	
+	
+	# Connect to the Vehicle
+	print "Connecting"
+	# referenced elsewhere in the file
+	global vehicle
+	
+	# once heartbeat has been adjusted, timeout needs to be set to 5
+	# location of USB connection to pixhawk
+	# connection_string       = '/dev/ttyACM0'
+	
+	connection_string = sitl.connection_string()
+	vehicle = connect(connection_string, wait_ready=True) #heartbeat_timeout = 30)
+	
+	print 'connected to vehicle'
+	
+	print "Basic pre-arm checks"
+	# Don't let the user try to fly while autopilot is booting
+	if vehicle.mode.name == "INITIALISING":
+		print "Waiting for vehicle to initialise"
+		time.sleep(1)
+	while vehicle.gps_0.fix_type < 2:
+		print "Waiting for GPS...:", vehicle.gps_0.fix_type
+		time.sleep(1)
+	
+	
+	# need to sort out home location setting
+	
+	# =============================================================================
+	#     # set home location
+	#     vehicle.home_location=vehicle.location.global_frame
+	# =============================================================================
+	
+	# Copter should arm in GUIDED mode
+	vehicle.mode    = VehicleMode("GUIDED")
 
 
-    # Sanity Checks
-    print " Type: %s" % vehicle._vehicle_type
-    print " Armed: %s" % vehicle.armed
-    print " System status: %s" % vehicle.system_status.state
-    print " GPS: %s" % vehicle.gps_0
-    print " Alt: %s" % vehicle.location.global_relative_frame.alt
-    print " Mode: %s" % vehicle.mode
-    print " Home Location: %s" % str(vehicle.home_location)
-    
-    
-    # completes command
-    print'DONE'
-    
-
+	# Sanity Checks
+	print " Type: %s" % vehicle._vehicle_type
+	print " Armed: %s" % vehicle.armed
+	print " System status: %s" % vehicle.system_status.state
+	print " GPS: %s" % vehicle.gps_0
+	print " Alt: %s" % vehicle.location.global_relative_frame.alt
+	print " Mode: %s" % vehicle.mode
+	print " Home Location: %s" % str(vehicle.home_location)
+	
+	
+	# completes command
+	print'DONE'
 
 def getHeading():
-    print(repr(vehicle.heading))
-    print 'DONE'
+	print(repr(vehicle.heading))
+	print 'DONE'
 
 def getPosition():
-    lat = float(vehicle.location.global_frame.lat)
-    lon = float(vehicle.location.global_frame.lon)
-    
-    try:
-        print drone_position_notify
-    except:
-        pass
-    # performs comparison between waypoint and current position
-    try:
-        lat_waypoint = float(drone_position_notify[0])
-        lon_waypoint = float(drone_position_notify[1])
-        lat_check = (lat_waypoint - lat)/lat_waypoint
-        lon_check = (lon_waypoint - lon)/lon_waypoint
-        
-        lat_percent = 5e-08
-        lon_percent = 5e-08
-        
-        if (lat_check < lat_percent and lon_check < lon_percent):
-            print 'NOTIFY'
-        else:
-            print 'not reached!' 
-    except:
-        print ' falied ' 
-        pass
-    print lat, lon
-    print 'DONE'
+	lat = float(vehicle.location.global_frame.lat)
+	lon = float(vehicle.location.global_frame.lon)
+	
+	print drone_position_notify
+
+	# check if waypoint reached
+	lat_waypoint = float(drone_position_notify[0])
+	lon_waypoint = float(drone_position_notify[1])
+	lat_check = (lat_waypoint - lat)/lat # use target position here - much less likely to = 0 
+	lon_check = (lon_waypoint - lon)/lon
+	
+	lat_percent = 5e-08
+	lon_percent = 5e-08
+	
+	if lat_check < lat_percent and lon_check < lon_percent:
+		print 'NOTIFY waypointReached'
+	else:
+		print 'WP not yet reached!'
+
+	# return position
+	print str(lat) + ' ' + str(lon)
+	print 'DONE'
 
 def getAltitude():
-    # returns altitude above mean sea level
-    alt = vehicle.location.global_frame.alt
-    print alt
-    print 'DONE'
+	# returns altitude above mean sea level
+	alt = vehicle.location.global_frame.alt
+	print alt
+	print 'DONE'
 
 def getStatus():
-    print vehicle.system_status
-    print 'DONE'
+	print vehicle.system_status
+	print 'DONE'
 
 def getHome():
-    print vehicle.home_location
-    print 'DONE'
+	print vehicle.home_location
+	print 'DONE'
 
 def setWaypoint(position):
-    try:
-        position[2]
-    except:
-        position.append(int(vehicle.location.global_frame.alt))
-    
-    lat, lon, alt = position
-    # convert to integers as input is 
-    lat = float(lat)
-    lon = float(lon) 
-    alt = float(alt)
-    
-    # saves current waypoint co-ordinates
-    global drone_position_notify
-    drone_position_notify = [lat, lon]
-    
-    
-    # converts to co-ord system relative to home point
-    point = LocationGlobalRelative(lat,lon,alt)
-    vehicle.simple_goto(point)
-    print 'moving to', lat, '', lon, '', alt
-    print 'DONE'
-    
+	# if no position given
+	try:
+		position[2]
+	except:
+		position.append(float(vehicle.location.global_frame.alt))
+	
+	lat, lon, alt = position
+	lat = float(lat)
+	lon = float(lon) 
+	alt = float(alt)
+	
+	# saves current waypoint co-ordinates
+	global drone_position_notify
+	drone_position_notify = [lat, lon]
+	
+	# converts to co-ord system relative to home point
+	point = LocationGlobalRelative(lat,lon,alt)
+	vehicle.simple_goto(point)
+	print 'moving to', lat, '', lon, '', alt
+	print 'DONE'
+	
 def setHeading(heading_relative):
-    # retrieves values froms args list
-    heading = int(heading_relative[0])
-    relative = False
-    # unsure about the behavour of this http://python.dronekit.io/guide/copter/guided_mode.html#guided-mode-copter-set-yaw
-    
-    if relative:
-        is_relative=1 #yaw relative to direction of travel
-    else:
-        is_relative=0 #yaw is an absolute angle
-    
-    # create the CONDITION_YAW command using command_long_encode()
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
-        0, #confirmation
-        heading,    # param 1, yaw in degrees
-        5,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
-        is_relative, # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
+	# retrieves values froms args list
+	heading = int(heading_relative[0])
+	relative = False
+	# unsure about the behavour of this http://python.dronekit.io/guide/copter/guided_mode.html#guided-mode-copter-set-yaw
+	
+	if relative:
+		is_relative=1 #yaw relative to direction of travel
+	else:
+		is_relative=0 #yaw is an absolute angle
+	
+	# create the CONDITION_YAW command using command_long_encode()
+	msg = vehicle.message_factory.command_long_encode(
+		0, 0,    # target system, target component
+		mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+		0, #confirmation
+		heading,    # param 1, yaw in degrees
+		5,          # param 2, yaw speed deg/s
+		1,          # param 3, direction -1 ccw, 1 cw
+		is_relative, # param 4, relative offset 1, absolute angle 0
+		0, 0, 0)    # param 5 ~ 7 not used
+	# send command to vehicle
+	vehicle.send_mavlink(msg)
+	vehicle.flush()
 
 def startTakeoffSequence():
-    # arming vehicle and making sure it is armed, if it fails then nothing else will work. 
-    for i in range(0,100):
-        vehicle.armed = True
-        print "Armed: %s" % vehicle.armed
-        if vehicle.armed == True:
-            break
-    
-    print 'TAKING OFF'
-    vehicle.simple_takeoff(10)
-    print 'DONE'
+	# arming vehicle and making sure it is armed, if it fails then nothing else will work. 
+	for i in range(0,100):
+		vehicle.armed = True
+		print "Armed: %s" % vehicle.armed
+		if vehicle.armed == True:
+			break
+	
+	print 'TAKING OFF'
+	vehicle.simple_takeoff(10)
+	print 'DONE'
 	# Set the controller to take-off and reach a safe altitude (e.g. 20ft)
 
 def startLandingSequence():
-    # should probably check the parameters for this..
-    vehicle.mode = VehicleMode("RTL")
-    print 'Return to Land executed'
-    print 'DONE'
+	# should probably check the parameters for this..
+	vehicle.mode = VehicleMode("RTL")
+	print 'Return to Land executed'
+	print 'DONE'
 
 
 def notification(fn):
-    # Notification functions
-    def location_callback(self, attr_name, value):
-        print "Location (Global): ", value
+	# Notification functions
+	def location_callback(self, attr_name, value):
+		print "Location (Global): ", value
 
-    if fn == 'LOCATION':
-        # Add a callback `location_callback` for the `global_frame` attribute.
-        vehicle.add_attribute_listener('location.global_frame', location_callback)
-    
+	if fn == 'LOCATION':
+		# Add a callback `location_callback` for the `global_frame` attribute.
+		vehicle.add_attribute_listener('location.global_frame', location_callback)
+	
 
 
 # end function
-    
-    
-
+	
 while 1:
-    # looks for command and optional arguments 
-    try:
-        cmd = raw_input('>').split()
-        if cmd[0] == "connection":
-            connection()
-        if cmd[0] == "getHeading":
-            getHeading()
-        if cmd[0] == "getPosition":
-            getPosition()
-        if cmd[0] == "getPosition":
-            getPosition()
-        if cmd[0] == "getAltitude":
-            getAltitude()
-        if cmd[0] == "setWaypoint":
-            setWaypoint(cmd[1:])
-        if cmd[0] == "setHeading":
-            setHeading(cmd[1:])
-        if cmd[0] == "startTakeoffSequence":
-            startTakeoffSequence()
-        if cmd[0] == "startLandingSequence":
-            startLandingSequence()
-        if cmd[0] == "getStatus":
-            getStatus()
-        if cmd[0] == "notification":
-            notification()
-        if cmd[0] == "getHome":
-            getHome()
-    #    if cmd == "telemetryTransmit":
-    #        telemetryTransmit(args)
-    
-        if cmd == "onActionCompleted":
-            print 'hi'
-        
-        if cmd == "exit":
-            # sitl stuff
-            try:
-                sitl.stop()
-            except:
-                print 'no sitl!'
-    except:
-        time.sleep(0.1)
+	# FCI-F = flight control interface, functions.py
+	args = raw_input('FCI-F> ').split()
+	cmd = args.pop(0) # remove first element (command)
+
+	print("COMMAND " + cmd)
+
+	# commands are exclusive so elif will be much faster than if
+
+	if cmd == "connection":
+		connection()
+	elif cmd == "getHeading":
+		getHeading()
+	elif cmd == "getPosition":
+		getPosition()
+	elif cmd == "getPosition":
+		getPosition()
+	elif cmd == "getAltitude":
+		getAltitude()
+	elif cmd == "setWaypoint":
+		setWaypoint(args)
+	elif cmd == "setHeading":
+		setHeading(args)
+	elif cmd == "startTakeoffSequence":
+		startTakeoffSequence()
+	elif cmd == "startLandingSequence":
+		startLandingSequence()
+	elif cmd == "getStatus":
+		getStatus()
+	elif cmd == "notification":
+		notification()
+	elif cmd == "getHome":
+		getHome()
+
+	# elif cmd == "telemetryTransmit":
+	#     telemetryTransmit(args)
+
+	elif cmd == "onActionCompleted":
+		print 'hi'
+	
+	elif cmd == "exit":
+		# sitl stuff
+		try:
+			sitl.stop()
+		except:
+			print 'no sitl!'
+
+		# exit by simply ending the script
+		break
+ 
+ 		# exit manually
+		sys.exit()
+
+	else:
+		print('Unkown command:', cmd)
+
+	# keep script alive in between commands
+	time.sleep(0.1)
--- dronekit_interface.py
+++ dronekit_interface.py
@@ -12,131 +12,198 @@ import time
 
 class FCInterface:
 
-    def __init__(self):
-        
-        # opens cli running python2 dronekit functions
-        self.py2 = subprocess.Popen(['python','-u','dronekit_functions.py'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
-        #self.py2 = subprocess.Popen([pycmd, '-u','dronekit_functions.py'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
-        print ("FCInterface initialised")
+	def __init__(self):
 
-    def interface(self, command):
-        """
-        pass a function name to dronekit_functions
-        returns single line string resulting from function. When DONE is passed function is considered complete and 
-        script moves on.
-        """
-        print('writing')
-        self.py2.stdin.write(command + '\n')
-        self.py2.stdin.flush()
-        print('reading')
-        line = 0
-        for i in range(0,100):
-            
-            self.waypoint_reached = False
-            
-            
-            # keeps last line printed
-            cmdreturn = line
-            read = self.py2.stdout.readline()
-            
-            if read.startswith('NOTIFY'):
-                try:
-                    self.waypoint_reached_fn()
-                except:
-                    pass
-                
-            print(read)
-            if read.startswith('DONE'):
-                return(cmdreturn)
-                break
+		self.timeoutLines = 1000
+		self.notificationCallbacks = {} 	# dictionary, populated by setNotificationCallback
+		self.notificationQueue = [] 		# last at end
+		
+		# opens cli running python2 dronekit functions
+		self.py2 = subprocess.Popen(['python2','-u','dronekit_functions.py'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
+		#self.py2 = subprocess.Popen([pycmd, '-u','dronekit_functions.py'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
+		
+		print ("FCInterface initialised")
 
-    def connection(self):
-        """
-        connection to vehicle through interface and performs basic setup including setting home.
-        won't complete until connection has complete
-        """
-        
-        self.interface('connection')
-        
-        return True
+	def interface(self, command):
+		"""
+		pass a function name to dronekit_functions
+		returns single line string resulting from function. When DONE is passed function is considered complete and 
+		script moves on.
+		"""
 
-    def getHeading(self):
-        """
-        no args, returns heading in degress from North
-        """
-        ans = self.interface('getHeading')
-        return ans 
-    
-    def getPosition(self):
-        """
-        no args, returns lat and lon as two vars
-        """
-        ans = self.interface('getPosition')
-        # converts single string to two ints
-        try:
-            position = ans.split()
-            lat = int(position[0])
-            lon = int(position[1])
-            return lat, lon, self.waypoint_reached
-        except:
-            return 0, 0, False 
-        
-    def getAltitude(self):
-        """
-        returns altitude above mean sea level in meters, no args
-        """
-        ans = self.interface('getAltitude')
-        return ans
+		print("")
+		print('Sending cmd:', command)
+		self.py2.stdin.write(command + '\n')
+		self.py2.stdin.flush()
+		print('Listening...')
 
-    def setWaypoint(self, lat, lon, alt):
-        """
-        uses simple_goto
-        """
-        self.interface('setWaypoint' + ' ' + str(lat) + ' ' + str(lon) + ' ' + str(alt))
+		returnLine = None 	# always the last line before the current one
+		numLinesRead = 0
+		stackHeight = 0 	# number of execution levels = number of COMMANDs printed - number of DONEs printed
+		while numLinesRead < self.timeoutLines:
+			# self.waypoint_reached = False     (no longer required)
+			
+			# check integrity of execution stack
+			if stackHeight == 0:
+				print("Erorr: stack height is non-zero = ", stackHeight)
 
-    def setHeading(self, heading, *args):
-        """
-        Pass a heading in degrees from North. I think if you pass True as a second argument the copter will return to 
-        using a relative heading (it may travel faster)
-        """
-        try: 
-            relative = str(args[0])
-        except:
-            relative = ''
-        self.interface('setHeading' + ' ' + str(heading) + ' ' + relative)
+			# read subprocess output
+			read = self.py2.stdout.readline()[:-1] # removes final newline character
+			
+			# print line through to interface
+			print(str(numLinesRead) + '| ' + read)
 
-    def startTakeoffSequence(self):
-        """
-        arms the copter then takes off to 10 meters
-        """
-        self.interface('startTakeoffSequence')
+			# handles
+			if read.startswith('NOTIFY'):
+				self.notificationQueue.append(read[7:])
 
-    def startLandingSequence(self):
-        """
-        starts landing copter
-        """
-        self.interface('startLandingSequence')
-        
-    def onActionCompleted(self, fn):
-        self.waypoint_reached_fn = fn
-        # Non-blocking callback function
-        # Called once by command.py during aircraft boot
-        # Sets up a notification so that, every time a commanded action is completed (e.g. waypoint/heading reached, take-off completed), function fn will be called-back
+				# (approach removed because it breaks the command cycle, complicating interpretation)
+				# try:
+				# 	self.waypoint_reached_fn()
+				# except:
+				# 	pass
 
-FCI = FCInterface()
+			elif read.startswith('COMMAND'):
+				stackHeight += 1
+
+			elif read.startswith('DONE'):
+				stackHeight -= 1
+				return returnLine
+				break
+
+			returnLine = read
+			numLinesRead += 1
+
+		# only reached if DONE is not returned
+		print("Command timed out after", numLinesRead, "lines read")
+
+	def setNotificationCallback(self, name, fn):
+		self.notificationCallbacks[name] = fn
+
+	def connection(self):
+		"""
+		connection to vehicle through interface and performs basic setup including setting home.
+		won't complete until connection has complete
+		"""
+		
+		self.interface('connection')
+		
+		return True
+
+	def getHeading(self):
+		"""
+		no args, returns heading in degress from North
+		"""
+		ans = self.interface('getHeading')
+		return ans 
+	
+	def getPosition(self):
+		"""
+		no args, returns lat and lon as two vars
+		"""
+		ans = self.interface('getPosition')
+
+		try:
+			position = ans.split()
+			lat = float(position[0])
+			lon = float(position[1])
+			return lat, lon
+		except:
+			return 0, 0
+		
+	def getAltitude(self):
+		"""
+		returns altitude above mean sea level in meters, no args
+		"""
+		ans = self.interface('getAltitude')
+		return ans
+
+	def setWaypoint(self, lat, lon, alt):
+		"""
+		uses simple_goto
+		"""
+		self.interface('setWaypoint' + ' ' + str(lat) + ' ' + str(lon) + ' ' + str(alt))
+
+	def setHeading(self, heading, *args):
+		"""
+		Pass a heading in degrees from North. I think if you pass True as a second argument the copter will return to 
+		using a relative heading (it may travel faster)
+		"""
+		try: 
+			relative = str(args[0])
+		except:
+			relative = ''
+		self.interface('setHeading' + ' ' + str(heading) + ' ' + relative)
+
+	def startTakeoffSequence(self):
+		"""
+		arms the copter then takes off to 10 meters
+		"""
+		self.interface('startTakeoffSequence')
+
+	def startLandingSequence(self):
+		"""
+		starts landing copter
+		"""
+		self.interface('startLandingSequence')
+		
+	# (superseded by notification system)
+	# def onActionCompleted(self, fn):
+	# 	self.waypoint_reached_fn = fn
+	# 	# Non-blocking callback function
+	# 	# Called once by command.py during aircraft boot
+	# 	# Sets up a notification so that, every time a commanded action is completed (e.g. waypoint/heading reached, take-off completed), function fn will be called-back
+
+# Testing Run
+# -----------
+
+numWPsDone = 0
+def waypointReachedCallback():
+	global numWPsDone, lat, lon
+
+	print("Waypoint reached (callback)")
+	numWPsDone += 1
+	print("WPs completed:", numWPsDone)
+
+	if numWPsDone >= 5:
+		print(numWPsDone, "WPs complete, starting landing...")
+		fci.startLandingSequence()
+	else:
+		print("Setting next WP...")
+		fci.setWaypoint(lat, lon + 0.0002, 300)
+
+# init interface
+fci = FCInterface()
+fci.setNotificationCallback('waypointReached', waypointReachedCallback) # set callback reference
 time.sleep(4)
 
-FCI.connection()
-FCI.startTakeoffSequence()
-time.sleep(10)
-FCI.getPosition()
-lat, lon, reached = FCI.getPosition()
+# [TODO] do simulator here instead
 
-FCI.setWaypoint(lat + 0.0001, lon, 600)
+# connect and take off
+fci.connection()
+fci.startTakeoffSequence()
+time.sleep(10)
+
+# get current position
+lat, lon = fci.getPosition()
+print('Initial position:', lat, lon)
+
+# set initial waypoint
+fci.setWaypoint(lat + 0.0001, lon, 600)
+
+# keep checking position and handling notifications
 for i in range(10000):
-    lat, lon, reached = FCI.getPosition()
-    time.sleep(0.25)
-    if reached:
-        print('reached')
-        break
-FCI.startLandingSequence()
+	# handle notifications
+	while len(fci.notificationQueue):
+		note = fci.notificationQueue.pop(0)
+		print("Handling notification", note)
+
+		if note in fci.notificationCallbacks:
+			fci.notificationCallbacks[note]()
+
+	# update position
+	lat, lon = fci.getPosition()
+	print('Pos checked cyclically:', lat, lon)
+
+	# wait
+	time.sleep(0.25)
