819fd2cccc1e077607a265c3781451b5fe9f4108
O Gent <38910643+o-gent@users.noreply.github.com>
2018-06-08 11:02:27 +0100

    setHeading work

--- dronekit_functions.py
+++ dronekit_functions.py
@@ -157,21 +157,16 @@ def setWaypoint(position):
 def setHeading(heading_relative):
     # retrieves values froms args list
     heading = int(heading_relative[0])
-    relative = False
-    # unsure about the behavour of this http://python.dronekit.io/guide/copter/guided_mode.html#guided-mode-copter-set-yaw
     
-    if relative:
-        is_relative=1 #yaw relative to direction of travel
-    else:
-        is_relative=0 #yaw is an absolute angle
-    
+    is_relative=0
+    yaw_speed = 30
     # create the CONDITION_YAW command using command_long_encode()
     msg = vehicle.message_factory.command_long_encode(
         0, 0,    # target system, target component
         mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
         0, #confirmation
         heading,    # param 1, yaw in degrees
-        5,          # param 2, yaw speed deg/s
+        yaw_speed,          # param 2, yaw speed deg/s
         1,          # param 3, direction -1 ccw, 1 cw
         is_relative, # param 4, relative offset 1, absolute angle 0
         0, 0, 0)    # param 5 ~ 7 not used
